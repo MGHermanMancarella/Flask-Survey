4335eb8d857a50f623d69b1956578c88fd4e04d1
Steven Lee <stevelee83@gmail.com>
2023-04-20 12:10:04 -0700

    finish step 2

--- app.py
+++ app.py
@@ -7,3 +7,19 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+reponses = []
+
+@app.get("/")
+def start_survey():
+    return render_template("survey_start.html",
+    survey_title=survey.title,
+    survey_instructions = survey.instructions)
+
+@app.post("/begin")
+def question_redirect():
+#     return redirect("/questions")
+
+# @app.get("/questions")
+# def populate_question():
+    return render_template("question.html")
