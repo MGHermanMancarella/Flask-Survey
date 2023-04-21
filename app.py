82e43eff149741690d8cc74c31a8be1f9acfb5ed
Michael <mherman315@gmail.com>
2023-04-20 17:35:25 -0700

    finished first half. cookie time

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 responses = []
-quest_num = 0
 
 @app.get("/")
 def start_survey():
@@ -21,14 +20,14 @@ def start_survey():
 @app.post("/begin")
 def question_redirect():
     """redirect to first question"""
+
     responses.clear()
     return redirect("/question/0")
 
 @app.get("/question/<int:q_num>")
-def populate_question(q_num):
+def populate_question(q_num=len(responses) ):
     """populate current iteration of question/answers"""
-    global quest_num
-    quest_num = q_num + 1
+
     answers = survey.questions[q_num].choices
     question = survey.questions[q_num]
     return render_template("question.html", question=question, choices=answers)
@@ -36,9 +35,10 @@ def populate_question(q_num):
 @app.post("/answer")
 def answer_redirect():
     """redirects and saves responses"""
-    global responses, quest_num
-    responses.append((survey.questions[quest_num-1].prompt, request.form['answer']))
-    if quest_num == len(survey.questions):
-        return render_template('completion.html', responses=responses)
+
+    responses.append(request.form['answer'])
+
+    if  len(responses) == len(survey.questions):
+        return render_template('completion.html', responses=responses, questions=survey.questions)
     else:
-        return redirect(f"/question/{quest_num}")
+        return redirect(f"/question/{len(responses) }")
