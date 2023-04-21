cb2562d6c110e9b0e8edce7bf34cd6e2a7bc6afb
Steven Lee <stevelee83@gmail.com>
2023-04-20 17:09:09 -0700

    finish step 5

--- app.py
+++ app.py
@@ -13,16 +13,20 @@ quest_num = 0
 
 @app.get("/")
 def start_survey():
+    """start the survey"""
+
     return render_template("survey_start.html",
     survey=survey)
-#inject survey
 
 @app.post("/begin")
 def question_redirect():
+    """redirect to first question"""
+    responses.clear()
     return redirect("/question/0")
 
 @app.get("/question/<int:q_num>")
 def populate_question(q_num):
+    """populate current iteration of question/answers"""
     global quest_num
     quest_num = q_num + 1
     answers = survey.questions[q_num].choices
@@ -31,12 +35,10 @@ def populate_question(q_num):
 
 @app.post("/answer")
 def answer_redirect():
-
+    """redirects and saves responses"""
     global responses, quest_num
-    responses.append((survey.questions[quest_num].prompt, request.form['answer']))
-    print('responses  ====== ', responses)
-    print("response tuple", responses[0][0])
+    responses.append((survey.questions[quest_num-1].prompt, request.form['answer']))
     if quest_num == len(survey.questions):
-        return render_template('completion.html')
+        return render_template('completion.html', responses=responses)
     else:
-        return redirect(f"/question/{quest_num}", responses=responses)
+        return redirect(f"/question/{quest_num}")
